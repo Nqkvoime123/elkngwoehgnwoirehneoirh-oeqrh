4027792313d72d8c4b71b44715e8638ee0234086
Nqkvoime123 <husein14tupev@gmail.com>
2022-01-13 11:00:30 +0200

    nfalsknfasl

---
+++ IdeaModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elkngwoehgnwoirehneoirh_oeqrh
+{
+    class IdeaModel
+    {
+        public string IdeaName
+        {
+            get => IdeaName;
+            set
+            {
+                if (value.Length > 15)
+                    throw new Exception("Imeto ne moje da e poveche ot 14 simvola");
+                IdeaName = value;
+            }
+        }
+        public string IdeaValue
+        {
+            get => IdeaValue;
+            set
+            {
+                if (value.Length < 9)
+                    throw new Exception("Stoinostta na ideqta trqbva da e nad 10 simvola");
+                IdeaValue = value;
+            }
+        }
+        string ideaName;
+        string ideaValue;
+
+        public int ValueCount;
+        public void CountValue()
+        {
+            ValueCount = ideaValue.Length;
+        }
+    }
+}
